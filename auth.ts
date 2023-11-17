453b4f34c6b9512449945950196fb905e33cefd0
Robert Fraser <99823868+RobFraserCodes@users.noreply.github.com>
2023-11-17 12:47:52 +0000

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -4,23 +4,29 @@ import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { adminDb } from "./firebase-admin";
 
+interface User {
+    id: number;
+    name: string;
+    email: string;
+  }
+  
 export const authOptions: NextAuthOptions = {
     providers: [
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                username: { label: "Username", type: "text", placeholder: "jsmith" },
-                password: { label: "Password", type: "password" },
-            },
-            async authorize(credentials) {
-                const user = { id: 1, name: "J Smith", email: "jsmith@example.com" };
-                if (user) {
-                    return user;
-                } else {
-                    return null;
-                }
-            }
-        }),
+        // CredentialsProvider({
+        //     name: "Credentials",
+        //     credentials: {
+        //         username: { label: "Username", type: "text", placeholder: "jsmith" },
+        //         password: { label: "Password", type: "password" },
+        //     },
+        //     async authorize(credentials) {
+        //         const user = { id: 1, name: "J Smith", email: "jsmith@example.com" };
+        //         if (user) {
+        //             return user;
+        //         } else {
+        //             return null;
+        //         }
+        //     }
+        // }),
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
